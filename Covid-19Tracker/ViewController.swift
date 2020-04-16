6dc75fe2788fc8211bdead0c7206130e60e71b68
Mayur Parmar <mrparmar2997@gmail.com>
2020-04-16 09:37:48 +0530

    Initial Commit

---
+++ Covid-19Tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Covid-19Tracker
+//
+//  Created by Mayur Parmar on 16/04/20.
+//  Copyright © 2020 Mayur Parmar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
